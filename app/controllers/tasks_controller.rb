bae55c7cfb5dd9ef0315aeb394f929c955fdd3bb
今永翔太 <>
2021-03-29 21:19:14 +0900

    3.新規投稿機能 new, create を実装

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,16 @@
 class TasksController < ApplicationController
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    Task.create(user_params)
+  end
+
+  private
+    def user_params
+      params.require(:task).permit(:title, :contents)
+
+    end
 end
